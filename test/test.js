f7d8ee95999bef18bef57a7816125c2e7c99b842
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-03-26 16:03:38 -0400

    Completed testing database setup

--- test/test.js
+++ test/test.js
@@ -6,42 +6,6 @@ const model = require('../model/index.js');
 // Consistency test - will ensure that whenever the value in column A or B is updated, the sum always remains 100. It won’t allow insertion/deletion/update in A or B if the total sum is anything other than 100.
 // Isolation test - will ensure that if two transactions are happening at the same time and trying to modify the data of the ACID test table, then these tractions are executing in isolation.
 // Durability test - will ensure that once a transaction over this table has been committed, it will remain so, even in the event of power loss, crashes, or errors.
-
-describe('Database Connection', () => {
-  beforeAll(() => {
-    dbConnection.connect();
-  });
-  afterAll(() => {
-    dbConnection.end();
-  });
-  test('Can connect to questions_answers database', () => {
-    expect(dbConnection.config.database).toBe('questions_answers');
-  });
-  test('Questions table contain field "asker_name" of type varchar', () => {
-    const cols = 'select * from information_schema.columns where table_schema="questions_answers" and table_name="questions" and column_name="asker_name";'
-    dbConnection.query(cols, (err, data) => {
-      expect(data[0].COLUMN_NAME).toBe('asker_name');
-      expect(data[0].DATA_TYPE).toBe('varchar');
-    })
-  });
-  test('Questions table contain field "body" of type varchar', () => {
-    const cols = 'select * from information_schema.columns where table_schema="questions_answers" and table_name="questions" and column_name="body";'
-    dbConnection.query(cols, (err, data) => {
-      expect(data[0].COLUMN_NAME).toBe('body');
-      expect(data[0].DATA_TYPE).toBe('varchar');
-    })
-  });
-  test('Questions table contain field "date_written" of type int', () => {
-    const cols = 'select * from information_schema.columns where table_schema="questions_answers" and table_name="questions" and column_name="date_written";'
-    dbConnection.query(cols, (err, data) => {
-      console.log(data);
-      expect(data[0].COLUMN_NAME).toBe('date_written');
-      expect(data[0].DATA_TYPE).toBe('int');
-    })
-  });
-});
-
-
 describe('Database Model Methods Exist', () => {
   test('getQuestions is a function', () => {
     expect(model.getQuestions).toBeInstanceOf(Function);
@@ -60,6 +24,72 @@ describe('Database Model Methods Exist', () => {
   });
   test('putReported is a function', () => {
     expect(model.putReported).toBeInstanceOf(Function);
+  });
+});
+
+describe('Database Setup', () => {
+  beforeAll(() => {
+    dbConnection.connect();
+  });
+  afterAll(() => {
+    dbConnection.end();
+  });
+  test('Can connect to questions_answers database', () => {
+    expect(dbConnection.config.database).toBe('questions_answers');
+  });
+  test('Questions table field "asker_name" is of type varchar', () => {
+    const cols = 'select * from information_schema.columns where table_schema="questions_answers" and table_name="questions" and column_name="asker_name";'
+    dbConnection.query(cols, (err, data) => {
+      expect(data[0].DATA_TYPE).toBe('varchar');
+    })
+  });
+  test('Questions table field "body" is of type varchar', () => {
+    const cols = 'select * from information_schema.columns where table_schema="questions_answers" and table_name="questions" and column_name="body";'
+    dbConnection.query(cols, (err, data) => {
+      expect(data[0].DATA_TYPE).toBe('varchar');
+    })
+  });
+  test('Questions table field "date_written" is of type int', () => {
+    const cols = 'select * from information_schema.columns where table_schema="questions_answers" and table_name="questions" and column_name="date_written";'
+    dbConnection.query(cols, (err, data) => {
+      expect(data[0].DATA_TYPE).toBe('int');
+    })
+  });
+  test('Questions table field "email" is of type varchar', () => {
+    const cols = 'select * from information_schema.columns where table_schema="questions_answers" and table_name="questions" and column_name="email";'
+    dbConnection.query(cols, (err, data) => {
+      expect(data[0].DATA_TYPE).toBe('varchar');
+    })
+  });
+  test('Questions table field "id" is of type varchar', () => {
+    const cols = 'select * from information_schema.columns where table_schema="questions_answers" and table_name="questions" and column_name="id";'
+    dbConnection.query(cols, (err, data) => {
+      expect(data[0].DATA_TYPE).toBe('varchar');
+    })
+  });
+  test('Questions table field "product_id" is of type int', () => {
+    const cols = 'select * from information_schema.columns where table_schema="questions_answers" and table_name="questions" and column_name="product_id";'
+    dbConnection.query(cols, (err, data) => {
+      expect(data[0].DATA_TYPE).toBe('int');
+    })
+  });
+  test('Questions table field "question_helpfulness" is of type int', () => {
+    const cols = 'select * from information_schema.columns where table_schema="questions_answers" and table_name="questions" and column_name="question_helpfulness";'
+    dbConnection.query(cols, (err, data) => {
+      expect(data[0].DATA_TYPE).toBe('int');
+    })
+  });
+  test('Questions table field "question_id" is of type int', () => {
+    const cols = 'select * from information_schema.columns where table_schema="questions_answers" and table_name="questions" and column_name="question_id";'
+    dbConnection.query(cols, (err, data) => {
+      expect(data[0].DATA_TYPE).toBe('int');
+    })
+  });
+  test('Questions table field "reported" is of type tinyint', () => {
+    const cols = 'select * from information_schema.columns where table_schema="questions_answers" and table_name="questions" and column_name="reported";'
+    dbConnection.query(cols, (err, data) => {
+      expect(data[0].DATA_TYPE).toBe('tinyint');
+    })
   });
 });
 
@@ -86,19 +116,5 @@ describe('Database Model Methods Exist', () => {
 
         //     })
         // })
-
-        // describe('Database Data Types', () => {
-        //   beforeAll(() => {
-        //     dbConnection.connect();
-        //   })
-        //   test('Questions table containers correct fields and datatypes', () => {
-        //     const firstRow = 'SELECT * FROM questions WHERE name =\'question_id\' LIMIT 1';
-        //     dbConnection.query()
-        //     expect(dbConnection.query(firstRow)).toEqual(typeof Number);
-        //   });
-        //   afterAll(() => {
-        //     dbConnection.end();
-        //   })
-        // });
 
 // select * from information_schema.columns where table_schema='questions_answers' and table_name='questions';
