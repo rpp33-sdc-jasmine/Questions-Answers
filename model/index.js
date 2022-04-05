70a354ffac1f6f8a89d86b556fef8da4b167137b
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-04-05 16:37:07 -0400

    Finished routes in server

--- model/index.js
+++ model/index.js
@@ -1,31 +1,39 @@
 const db = require('../db').dbConnection;
-// CREATE TABLE answers (
-//   id INT NOT NULL,
-//   question_id INT NOT NULL,
-//   body VARCHAR(1000),
-//   date INT(11),
-//   answerer_name VARCHAR(30),
-//   helpfulness INT,
-//   reported BOOLEAN
+
 module.exports= {
   getQuestions: (id) => {
     db.connect();
-    // const questionQuery = `SELECT question_id, question_body, question_date, asker_name, reported, question_helpfulness FROM questions WHERE product_id=${id};`;
-    const questionQuery = `SELECT questions.question_id, questions.question_body, questions.question_date, questions.asker_name, questions.reported, questions.question_helpfulness, answers.id, answers.body, answers.date, answers.answerer_name, answers.helpfulness
-    FROM questions INNER JOIN answers
-    ON answers.question_id = questions.question_id
-    AND questions.product_id=${id};`;
-    const options = { sql: questionQuery, nestTables: true };
+    const questionQuery = `SELECT question_id, question_body, question_date, asker_name, reported, question_helpfulness FROM questions WHERE questions.product_id=${id}`
+
     return new Promise((resolve, reject) => {
-      db.query(options, (err, rows) => {
+      db.query(questionQuery, (err, rows) => {
         if (err) {
-          console.log('Error retrieving questions from the db', err);
+          console.log('Error retrieving questions from database', err);
         } else {
-          resolve({product_id: id, results: rows});
+          resolve({product_id: id, results: rows})
         }
       })
     })
-    db.end();
+  },
+  getAnswers: (questions) => {
+    // db.connect();
+    const answerQuery = `SELECT answers.id, answers.body, answers.date, answers.answerer_name, answers.helpfulness FROM answers WHERE`;
+    return new Promise((resolve, reject) => {
+      resolve(questions.map((question) =>{
+        const answerQuery = `SELECT id, body, date, answerer_name, reported, helpfulness FROM answers WHERE question_id=${question.question_id};`;
+        db.query(answerQuery, (err, rows) => {
+          if (err) {
+            console.log('Error retrieving questions from the db', err);
+          } else {
+            return question.answers = rows;
+          }
+        });
+      }));
+    })
+    // db.end();
+  },
+  getPhotos: () => {
+    //TODO
   },
   postQuestion: (question, callback) => {
     //TODO: question ID needs to autoincremented from the last question somehow
@@ -38,22 +46,7 @@ module.exports= {
           console.log('Success inserting data into db', data);
           callback(null, data)
       }
-  })
-  },
-  getAnswers: (id) => {
-    // db.connect();
-    const answerQuery = `SELECT id, body, date, answerer_name, reported, helpfulness FROM answers WHERE question_id=${id};`;
-    return new Promise((resolve, reject) => {
-      db.query(answerQuery, (err, answers) => {
-        if (err) {
-          console.log('Error retrieving questions from the db', err);
-        } else {
-          let currentId = answers.id
-          resolve(...answers);
-        }
-      })
     })
-    // db.end();
   },
   postAnswer: (id) => {
 
--- server/index.js
+++ server/index.js
@@ -1,44 +1,68 @@
 const express = require('express');
 const db = require('../db/index.js').dbConnection;
 const models = require('../model/index.js');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 4000;
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  const id = 1;
-  models.getQuestions(id)
-  .then((response) => {
-    res.status(200).send(response)
-  })
-  .catch((err) => {
-    console.log('Error Retrieving Questions and Answers from Database', err);
-  })
+app.get('/qa/questions', (req, res) => {
+  const params = {
+    id: req.query.product_id,
+    page: req.query.page,
+    count: req.query.count
+  }
+    res.status(200).send(params);
 });
-app.get('/question', (req, res) => {
-  let question = req.query;
-  models.postQuestion(question, (err, data) => {
-    if (err) {
-      console.log('Error inserting question into db', err)
-    } else {
-      console.log('Success inserting data into db', data)
-    }
-  });
-  res.end('Question posted');
+
+app.get('/qa/questions/:question_id/answers', (req, res) => {
+  const id = req.params.question_id
+  res.status(200).send(id);
 });
+
+app.post('/qa/questions', (req, res) => {
+  data = {
+    body: req.query.body,
+    name: req.query.name,
+    email: req.query.email,
+    product_id: req.query.product_id
+  }
+  res.status(201).send(data);
+});
+
+app.post('/qa/questions/:question_id/answers', (req, res) => {
+  data = {
+    body: req.query.body,
+    name: req.query.name,
+    email: req.query.email,
+    photos: req.query.photos,
+    question_id: req.params.question_id
+  }
+  res.status(201).send(data);
+});
+
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  id = req.params.question_id;
+  res.status(201).send('Success Updating Question Helpfulness');
+})
+
+app.put('/qa/questions/:question_id/report', (req, res) => {
+  id = req.params.question_id;
+  res.status(201).send('Success Reporting Question');
+})
+
+app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+  id = req.params.answer_id;
+  res.status(201).send('Success Updating Answer Helpfulness');
+})
+
+app.put('/qa/answers/:answer_id/report', (req, res) => {
+  id = req.params.answer_id;
+  res.status(201).send('Success Reporting Answer');
+})
 
 app.listen(port, () => {
   console.log(`Listening at Localhost: ${port}`);
 });
-
-
-// app.get('/', (req, res) => {
-//   const id = 1;
-//   models.getQuestions(id, (results) => {
-//     let data = {
-//       product_id: id,
-//       results: results
-//     }
-//     res.status(200).send(data);
-//   });
-// });
