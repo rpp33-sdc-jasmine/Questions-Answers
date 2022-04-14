618bae411bc7f5f210dd1dbd2c0b0406bac5a61a
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-04-14 18:16:30 -0400

    Added indexing

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,10 @@
+DROP DATABASE IF EXISTS questions_answers;
+
 CREATE DATABASE questions_answers;
 
 USE questions_answers
 
-DROP TABLE IF EXISTS questions;
+
 CREATE TABLE questions (
   question_id INT NOT NULL,
   product_id INT NOT NULL,
@@ -11,10 +13,12 @@ CREATE TABLE questions (
   asker_name VARCHAR(30),
   email VARCHAR(50),
   question_helpfulness INT,
-  reported BOOLEAN
+  reported BOOLEAN,
+  PRIMARY KEY (question_id)
 );
 
-DROP TABLE IF EXISTS answers;
+CREATE INDEX questions ON questions (product_id, question_id);
+
 CREATE TABLE answers (
   id INT NOT NULL,
   question_id INT NOT NULL,
@@ -23,15 +27,20 @@ CREATE TABLE answers (
   answerer_name VARCHAR(30),
   email VARCHAR(30),
   helpfulness INT,
-  reported BOOLEAN
+  reported BOOLEAN,
+  PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS photos;
+CREATE INDEX answers ON answers (question_id, id);
+
 CREATE TABLE photos (
   photo_id INT NOT NULL,
   answer_id INT NOT NULL,
-  url VARCHAR(300)
+  url VARCHAR(300),
+  PRIMARY KEY (photo_id)
 );
+
+CREATE INDEX photos ON photos (answer_id, photo_id);
 
 LOAD DATA LOCAL INFILE '/Users/caitlinwinters/Desktop/Questions-Answers-Data/questions.csv'
 INTO TABLE questions
@@ -40,20 +49,12 @@ OPTIONALLY ENCLOSED BY '\"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-ALTER TABLE questions ADD COLUMN id_key varchar(36) NOT NULL;
-UPDATE questions SET id_key = UUID();
-ALTER TABLE questions ADD PRIMARY KEY (id_key);
-
 LOAD DATA LOCAL INFILE '/Users/caitlinwinters/Desktop/Questions-Answers-Data/answers.csv'
 INTO TABLE answers
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '\"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
-
-ALTER TABLE answers ADD COLUMN id_key varchar(36) NOT NULL;
-UPDATE answers SET id_key = UUID();
-ALTER TABLE answers ADD PRIMARY KEY (id_key);
 
 LOAD DATA LOCAL INFILE '/Users/caitlinwinters/Desktop/Questions-Answers-Data/answers_photos.csv'
 INTO TABLE photos
@@ -62,6 +63,7 @@ OPTIONALLY ENCLOSED BY '\"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-ALTER TABLE photos ADD COLUMN id_key varchar(36) NOT NULL;
-UPDATE photos SET id_key = UUID();
-ALTER TABLE photos ADD PRIMARY KEY (id_key);
+ALTER TABLE questions MODIFY COLUMN question_id INT auto_increment;
+ALTER TABLE answers MODIFY COLUMN id INT auto_increment;
+ALTER TABLE photos MODIFY COLUMN photo_id INT auto_increment;
+
