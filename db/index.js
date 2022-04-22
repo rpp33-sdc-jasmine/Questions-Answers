ceeb960e3f2f2c2edb0066d14697c8f75d8c7d8d
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-04-22 11:35:51 -0400

    Forgot comma

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const dbConnection = mysql.createConnection({
   host: '44.200.37.117', //needs to be,
-  user: 'remote-server'
+  user: 'remote-server',
   password: 'USMhorse17!',
   database: 'questions_answers'
 });
