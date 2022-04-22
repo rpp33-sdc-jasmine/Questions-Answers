d4e3caa080f494e350ce2bd12942d2191f906475
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-04-22 11:16:38 -0400

    Fixed typo

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const dbConnection = mysql.createConnection({
   // host: 'localhost',
-  host: 'ec2-3-80-177-122.compute-1.amazonaws.com'
+  host: 'ec2-3-80-177-122.compute-1.amazonaws.com',
   user: 'root', //change to a new user that is associated with my server ip and has been granted appropriate privilages?
   password: '',
   database: 'questions_answers'
