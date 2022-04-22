b51c07418cf15b7ba615f5a870f30b18803f11b8
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-04-22 11:15:52 -0400

    Updated host address

--- db/index.js
+++ db/index.js
@@ -1,7 +1,9 @@
 const mysql = require('mysql2');
 
 const dbConnection = mysql.createConnection({
-  user: 'root',
+  // host: 'localhost',
+  host: 'ec2-3-80-177-122.compute-1.amazonaws.com'
+  user: 'root', //change to a new user that is associated with my server ip and has been granted appropriate privilages?
   password: '',
   database: 'questions_answers'
 });
