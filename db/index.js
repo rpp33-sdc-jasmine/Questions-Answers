58f288d9e44c161b5890dd445530b82081c2e114
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-04-22 15:51:28 -0400

    Deleted space

--- db/index.js
+++ db/index.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 
 const dbConnection = mysql.createConnection({
-  // host: '172.31.24.98',
-  host: ' 3.80.177.122',
+  host: '172.31.24.98',
+  host: '3.80.177.122',
   user: 'sdc',
   password: 'SDCpassword!0@',
   database: 'questions_answers'
