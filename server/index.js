749e1e285d0979904b824aaa3e784c15ea8fd8e2
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-04-26 21:55:43 -0400

    removed console.log

--- server/index.js
+++ server/index.js
@@ -19,7 +19,6 @@ const makeApp = function(models) {
     client.get('product' + req.query.product_id)
     .then((result) => {
       if(result !== null){
-        console.log('data accessed from cache!');
         res.send(JSON.parse(result));
       } else {
         next();
