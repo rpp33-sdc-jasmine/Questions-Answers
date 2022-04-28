e3492c5e943f10f877ee2e68f22652db111e5d06
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-04-28 13:12:44 -0400

    Started new relic

--- newrelic.js
+++ newrelic.js
@@ -22,7 +22,7 @@ exports.config = {
    * this feature: https://docs.newrelic.com/docs/transition-guide-distributed-tracing
    * Default is false.
    */
-  agent_enabled: 'NEW_RELIC_ENABLED=false',
+  agent_enabled: 'NEW_RELIC_ENABLED',
   distributed_tracing: {
     /**
      * Enables/disables distributed tracing.
