44cde81682f2e69f4488b93ede53c9a01896e983
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-05-04 15:11:26 -0400

    Turned off new relic

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const newrelic = require('newrelic')
+// const newrelic = require('newrelic')
 const express = require('express');
 // const redis = require('redis');
 const bodyParser = require('body-parser');
